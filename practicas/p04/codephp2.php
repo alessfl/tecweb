598632896d4eb86a69022326a1f0aaff0b7a4840
Alessandra Flores Hernández <alessfl@MacBook-Air-de-Alessandra.local>
2025-09-04 19:10:55 -0600

    p04: todos los códigos han sido arreglados y verificados

--- practicas/p04/codephp2.php
+++ practicas/p04/codephp2.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<?php ← (1)
+<?php
 $variable1=" PHP 5";
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<?php ← (2)
+<?php
 echo "<title>Una pagina llena de scripts PHP</title>";
 ?>
 </head>
+
--- practicas/p04/config/principal.php
+++ practicas/p04/config/principal.php
@@ -1,6 +1,6 @@
 <?php
 include("encabezado.inc.php");  // abre <!DOCTYPE>, <html>, <head>, <body>
 include("cuerpo.inc.php");      // solo contenido <h1>, <h2> ...
-    require("cuerpo.html"); // solo contenido HTML
+    require("cuerpo.html"); // solo contenido HTML ...  se soluciona el error de las etiquetas 
 include("pie.inc.php");         // cierra <body>, </html>
 ?>

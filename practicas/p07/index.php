fa4e3931d650a58f2bdbd8eb36a68a4e4fe3848b
Alessandra Flores Hernández <alessfl@MacBook-Air-de-Alessandra.local>
2025-09-26 11:03:35 -0600

    p07: se agregó ejercicio 2 generación repetitiva de 3 numeros aleatorios

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -1,3 +1,4 @@
+<?php include("src/funciones.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,24 +10,17 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+        if (isset($_GET['numero'])) {
+            echo "<h3>R= " . esMultiploDe5y7($_GET['numero']) . "</h3>";
         }
     ?>
 
     <h2>Ejercicio 2</h2>
     <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una secuencia</p>
     <?php
-
+        if (isset($_GET['ej2'])) {
+            echo generarSecuenciaImparParImpar();
+        }
     ?>
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
